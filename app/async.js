8cad67a0e2ad4fc0e09dd8818048c3e2b1535f61
Chris Kwan <chriswkwan@gmail.com>
2015-10-26 23:23:44 -0400

    Add answers for async behavior

--- app/async.js
+++ app/async.js
@@ -2,10 +2,31 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+  	return {
+  		then: function(callback) {
+  			callback(value);
+  		}
+  	};
   },
 
   manipulateRemoteData : function(url) {
-
+	return {
+		then: function(callback) {
+	  		var httpRequest = new XMLHttpRequest();
+	  		httpRequest.onreadystatechange = function() {
+	  			if (httpRequest.readyState === XMLHttpRequest.DONE) {
+	  				var people = JSON.parse(httpRequest.response).people;
+	  				var names = [];
+		  			for (var i=0; i<people.length; i++) {
+		  				names.push(people[i].name);
+		  			}
+		  			names.sort();
+		  			callback(names);
+		  		}
+	  		};
+	  		httpRequest.open("GET", url, true);
+	  		httpRequest.send(null);
+		}
+	};
   }
 };
