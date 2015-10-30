c4e7da31dea490e323b96e8a18659d8d9c22bdf7
Chris Kwan <chriswkwan@gmail.com>
2015-10-30 00:06:45 -0400

    Add first 2 answers for numbers: binary operations

--- app/numbers.js
+++ app/numbers.js
@@ -2,15 +2,17 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+  	var bin = num.toString(2);
+  	var index = bin.length - bit;
+  	return Number.parseInt(bin[index], 10);
   },
 
   base10: function(str) {
-
+  	var bin = Number.parseInt(str, 2);
+  	return Number.parseInt(bin, 10);
   },
 
   convertToBinary: function(num) {
-
   },
 
   multiply: function(a, b) {
