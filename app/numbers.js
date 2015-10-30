35f91dfe7c5657bd67e3d4a2c77fa1abb1eb0f0d
Chris Kwan <chriswkwan@gmail.com>
2015-10-30 08:16:42 -0400

    Add answers for numbers: binary operations and decimals

--- app/numbers.js
+++ app/numbers.js
@@ -13,9 +13,27 @@ exports.numbersAnswers = {
   },
 
   convertToBinary: function(num) {
+    var bin = num.toString(2);
+
+    // Add leading 0's until string is length 8
+    // (not sure if this is what they want though)
+    while (bin.length < 8) {
+      bin = "0" + bin;
+    }
+
+    return bin;
   },
 
   multiply: function(a, b) {
+    // Precision of numbers is number of digits
+    // after the decimal point
+    var aStr = a.toString();
+    var aPrecision = aStr.slice(aStr.indexOf(".") + 1).length;
+    var bStr = b.toString();
+    var bPrecision = bStr.slice(bStr.indexOf(".") + 1).length;
 
+    var maxPrecision = Math.max(aPrecision, bPrecision);
+    var preciseStr = (a * b).toPrecision(maxPrecision);
+    return Number.parseFloat(preciseStr);
   }
 };
