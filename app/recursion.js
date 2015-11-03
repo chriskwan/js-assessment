8ffdc0227b5ef1f5b92ffc7a91475aa9cd530ada
Christopher Kwan <ckwan@vistaprint.com>
2015-11-03 13:55:44 -0500

    Add answers for permutations: validParentheses

--- app/recursion.js
+++ app/recursion.js
@@ -44,6 +44,35 @@ exports.recursionAnswers = {
   },
 
   validParentheses: function(n) {
+    if (n < 1) {
+      return [];
+    } else if (n === 1) {
+      return ['()'];
+    } else if (n === 2) {
+      return ['(())', '()()'];
+    } else {
+      // use a map instead of an array to prevent duplicates
+      // e.g.: "()"+"()()" === "()()"+"()"
+      var comboMap = {};
+      var innerCombos = this.validParentheses(n-1);
 
+      // For each inner combo, add one more pair of parentheses to it
+      // in each of the 3 valid configurations
+      for (var i=0; i<innerCombos.length; i++) {
+        var curInnerCombo = innerCombos[i];
+
+        comboMap["(" + curInnerCombo + ")"] = true;
+        comboMap["()" + curInnerCombo] = true;
+        comboMap[curInnerCombo + "()"] = true;
+      }
+
+      var comboList = [];
+      for (var combo in comboMap) {
+        if (comboMap.hasOwnProperty(combo)) {
+          comboList.push(combo);
+        }
+      }
+      return comboList;
+    }
   }
 };
