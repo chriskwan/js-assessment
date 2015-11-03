832f7b749395645fba826f44693b6d5c82217ac5
Chris Kwan <chriswkwan@gmail.com>
2015-11-03 07:44:21 -0500

    Add answer for strings: reduceString

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+  	var newStr = "";
+  	var prevLetter;
+  	var curLetter;
+  	var curCount = 0;
+  	for (var i=0; i<str.length; i++) {
+  		var curLetter = str[i];
+  		// Looking at a currently repeated letter
+  		// or the first letter
+  		if (curLetter === prevLetter || !prevLetter) {
+  			if (curCount < amount) {
+  				newStr += curLetter;
+  			}
+			curCount++;
+  		} else { // Looking at a new letter
+  			newStr += curLetter;
+  			curCount = 1;
+  		}
 
+		prevLetter = curLetter;
+  	}
+  	return newStr;
   },
   wordWrap: function(str, cols) {
 
