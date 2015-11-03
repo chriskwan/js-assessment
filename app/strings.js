9134adaef95b5ee48291266c7fe35230f2d6cdb1
Chris Kwan <chriswkwan@gmail.com>
2015-11-03 08:12:29 -0500

    Add answer for strings: wordWrap

--- app/strings.js
+++ app/strings.js
@@ -25,7 +25,34 @@ exports.stringsAnswers = {
   	return newStr;
   },
   wordWrap: function(str, cols) {
+  	var newStr = "";
+  	var words = str.split(" ");
+  	var curLine = "";
 
+  	for (var i=0; i<words.length; i++) {
+  		// first word in the line
+  		if (!curLine.length) {
+  			curLine += words[i];
+  		} else {
+  			// What the current line length would be if the current word as added
+  			// including the space in between the words
+  			var lineLengthWithWord = curLine.length + words[i].length + 1;
+  			if (lineLengthWithWord <= cols) {
+  				curLine += " " + words[i];
+  			} else { // current word needs to go on a new line
+  				newStr += curLine + "\n";
+  				curLine = "";
+  				curLine += words[i];
+  			}
+  		}
+  	}
+
+  	// handle the last word
+  	if (curLine) {
+  		newStr += curLine;
+  	}
+
+  	return newStr;
   },
   reverseString: function(str) {
 
